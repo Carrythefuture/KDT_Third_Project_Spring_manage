e5f08bae2c1be64c0c9247feff379663e5bab5c3
Carrythefuture <146246027+Carrythefuture@users.noreply.github.com>
2025-12-05 12:00:38 +0900

    chart system -ing

--- src/main/java/com/tnt/app/dao/ChartDAO.java
+++ src/main/java/com/tnt/app/dao/ChartDAO.java
@@ -16,15 +16,38 @@ public class ChartDAO {
 	 public Map<String, List<Map<String,Object>>>  getData() {
 	     Map<String, List<Map<String,Object>>> map = new HashMap<String, List<Map<String,Object>>>();
 		 
-	     
+//	     연령대
 	     List<Map<String,Object>> list = mybatis.selectList("Chart.getAgeData");
 	     
-	     for (Map<String,Object> row : list) {
+//	     for (Map<String,Object> row : list) {
+//	    	    Object label = row.get("LABEL");
+//	    	    Object cnt = row.get("CNT");
+//	    	    System.out.println("label = " + label + ", cnt = " + cnt);
+//	    	}
+	     map.put("age", list);
+	     
+//	     성별
+	     List<Map<String,Object>> genderList = mybatis.selectList("Chart.getGenderData");
+	     
+//	     for (Map<String,Object> row : genderList) {
+//	    	    Object label = row.get("GENDER");
+//	    	    Object cnt = row.get("CNT");
+//	    	    System.out.println("label = " + label + ", cnt = " + cnt);
+//	    	}
+	     
+	     map.put("gender", genderList);
+	    
+	     
+//	     성별
+	     List<Map<String,Object>> bodyList = mybatis.selectList("Chart.getBodyData");
+	     
+	     for (Map<String,Object> row : bodyList) {
 	    	    Object label = row.get("LABEL");
 	    	    Object cnt = row.get("CNT");
 	    	    System.out.println("label = " + label + ", cnt = " + cnt);
 	    	}
-	     map.put("age", list);
+	     
+	     map.put("bodyType", bodyList);
 	     
 	     
 		 return map;
